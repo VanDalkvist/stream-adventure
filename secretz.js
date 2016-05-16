339ce6f47f750ae3f8764637b1ed88a87b5d2d47
VanDalkvist <i_van@list.ru>
2016-05-16 11:57:28 +0300

    secretz

--- secretz.js
+++ secretz.js
@@ -2,7 +2,6 @@
 
 var crypto = require('crypto');
 var concat = require('concat-stream');
-var through = require('through2');
 var zlib = require('zlib');
 var tar = require('tar');
 
@@ -12,11 +11,14 @@ module.exports = {};
 
 // initialization
 
-var entries = {};
-
 var parser = tar.Parse();
 parser.on('entry', function (entry) {
-    entries[entry.path] = entry;
+    if (entry.type !== 'File') return;
+
+    var hash = crypto.createHash('md5', { encoding: 'hex' });
+    entry.pipe(hash).pipe(concat(function (hash) {
+        console.log(hash + ' ' + entry.path);
+    }));
 });
 
 var decryptStream = crypto.createDecipher(process.argv[2], process.argv[3]);
@@ -24,34 +26,4 @@ var decryptStream = crypto.createDecipher(process.argv[2], process.argv[3]);
 process.stdin
     .pipe(decryptStream)
     .pipe(zlib.createGunzip())
-    .pipe(parser)
-    .pipe(through(function () {
-    }, function _end(next) {
-        var res = this;
-
-        for (var key in entries) {
-            if (entries.hasOwnProperty(key)) {
-                var hash = crypto.createHash('md5', { encoding: 'hex' });
-
-                var entry = entries[key];
-
-                entry.on('readable', updateHash(entry, key, hash, res));
-            }
-        }
-
-        function updateHash(entry, key, hash, res) {
-            return function () {
-                var data = entry.read();
-                if (data)
-                    hash.update(data);
-                else {
-                    res.push(hash.digest('hex') + ' ' + key + '\n');
-                }
-            }
-        }
-
-        next();
-    }))
-    .pipe(process.stdout);
-
-// private methods
+    .pipe(parser);
